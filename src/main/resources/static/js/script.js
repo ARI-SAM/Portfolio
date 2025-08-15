95cdc62f70af9cd190bb0ad17aa49d1ef321ab22
ARI-SAM <riku9748759296@gmail.com>
2025-08-15 23:52:56 +0530

    made more changes in website made responsive

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const navToggle = document.getElementById('navToggle');
-  const navLinks = document.getElementById('navLinks');
-  navToggle && navToggle.addEventListener('click', ()=>{
-    const isOpen = navLinks.style.display === 'flex';
-    navLinks.style.display = isOpen ? 'none' : 'flex';
-  });
+document.getElementById("navToggle").addEventListener("click", function() {
+    document.getElementById("navLinks").classList.toggle("hidden");
+    document.getElementById("navLinks").classList.toggle("show");
 });
