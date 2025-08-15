d3ede6fc49a27f7ac8e8bebba44dffaea9ba95f9
ARI-SAM <ARI-SAM@users.noreply.github.com>
2025-08-15 18:32:59 +0000

    deploy: 20846ac064ddb9aa53ab9ffb5c614705866be678

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("navToggle").addEventListener("click", function() {
+    document.getElementById("navLinks").classList.toggle("hidden");
+    document.getElementById("navLinks").classList.toggle("show");
+});
